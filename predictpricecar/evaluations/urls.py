4814113943c7a065fc74f442b33732299d0c0325
pdrgrds <pdrgrds@hotmail.com>
2024-11-30 08:36:51 -0500

    Feat: add get detail service by id in predicction

--- predictpricecar/evaluations/urls.py
+++ predictpricecar/evaluations/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import VehicleEvaluationListView, PublicationListView
+from .views import VehicleEvaluationListView, VehicleEvaluationDetailView, PublicationListView
 
 urlpatterns = [
     path('evaluations/', VehicleEvaluationListView.as_view(), name='evaluation-list'),
+    path('evaluations/<int:pk>/', VehicleEvaluationDetailView.as_view(), name='evaluation-detail'),
     path('publications/', PublicationListView.as_view(), name='publication-list'),
 ]
--- predictpricecar/evaluations/views.py
+++ predictpricecar/evaluations/views.py
@@ -6,6 +6,10 @@ class VehicleEvaluationListView(generics.ListCreateAPIView):
     queryset = VehicleEvaluation.objects.all()
     serializer_class = VehicleEvaluationSerializer
 
+class VehicleEvaluationDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = VehicleEvaluation.objects.all()
+    serializer_class = VehicleEvaluationSerializer
+
 class PublicationListView(generics.ListCreateAPIView):
     queryset = Publication.objects.all()
     serializer_class = PublicationSerializer
